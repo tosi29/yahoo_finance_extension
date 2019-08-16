e03dc4da617da08a236a5831343db83b8341bc3c
tosi29 <tosi16@gmail.com>
2019-08-16 17:07:54 +0900

    Update package script

--- package.sh
+++ package.sh
@@ -1,3 +1,16 @@
 #! /bin/sh
 
-zip yahoo_finance_extension.zip css js icon manifest.json
+rm -rf dist
+mkdir -p dist dist/js
+
+cp -r css dist
+cp -r icon dist
+cp manifest.json dist
+
+uglifyjs --compress --mangle -- js/script.js > dist/js/script.js
+uglifyjs --compress --mangle -- js/tablesort_custom.js > dist/js/tablesort_custom.js
+
+(
+    cd dist
+    zip yahoo_finance_extension.zip *
+)
