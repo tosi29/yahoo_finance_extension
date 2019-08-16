6d6abcf59d030fddf1b15f551e0503542c62c171
tosi29 <tosi16@gmail.com>
2019-08-16 11:23:40 +0900

    Add dividend_style

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ for(var i=0; i<td_all.length; i++) {
 	//テキストを取得
 	var text = td_all[i].innerText;
 	
-	td_all[i] = percent_style(td_all[i])
+	td_all[i] = percent_style(td_all[i]);
+	td_all[i] = dividend_style(td_all[i]);
 
 	//倍率表記のマッチ（貸借倍率用）
 	if (text.match(/^0\.([0-9]{1,})倍/)) {
@@ -97,6 +98,34 @@ function percent_style(element) {
 		}
 		
 		element.setAttribute("bgcolor", bgcolor);
+	}
+	return element;
+}
+
+
+function dividend_style(element) {
+	var text = element.innerText;
+
+	if (text.match(/^([0-9]{1,})\.[0-9]{1,}%/)) {
+		var percent = parseInt(RegExp.$1);
+		var color;
+
+		if (percent < 1) {
+			color = "FF";
+		}
+
+		if (percent >= 1) {
+			color = "EE";
+		}
+		
+		if (percent > 3) {
+			color = "CC";
+		}
+
+		if (percent > 5) {
+			color = "AA";
+		}
+		element.setAttribute("bgcolor", "#" + color + color + "FF");
 	}
 	return element;
 }
