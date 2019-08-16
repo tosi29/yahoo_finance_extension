ea96954d17f058d2b1a00f817bb6c303cb28255e
tosi29 <tosi16@gmail.com>
2019-08-16 12:00:13 +0900

    Refine gradation logic

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ var td_all = table_div.getElementsByTagName("td");
 var tables = table_div.getElementsByTagName("table");
 new Tablesort(tables[0]);
 
+
+
 for(var i=0; i<td_all.length; i++) {
 	
 	//テキストを取得
 	var text = td_all[i].innerText;
 	
-	td_all[i] = percent_style(td_all[i]);
+	td_all[i] = signed_percent_style(td_all[i]);
 	td_all[i] = dividend_style(td_all[i]);
 
 	//倍率表記のマッチ（貸借倍率用）
@@ -18,33 +20,7 @@ for(var i=0; i<td_all.length; i++) {
 		var color;
 		var bgcolor;
 
-		if (percent > 99) {
-			color = "FF";
-		}
-
-		if (percent <=99) {
-			color = "EE";
-		}
-		
-		if (percent < 80) {
-			color = "CC";
-		}
-
-		if (percent < 60) {
-			color = "AA";
-		}
-
-		if (percent < 45) {
-			color = "88";
-		}
-
-		if (percent < 30) {
-			color = "66";
-		}
-
-		if (percent < 15) {
-			color = "44";
-		}
+		color = gradation_desc(percent, 100);
 		bgcolor = "#" + "FF" + color + color;
 		td_all[i].setAttribute("bgcolor", bgcolor);
 	}
@@ -52,42 +28,13 @@ for(var i=0; i<td_all.length; i++) {
 }
 
 
-function percent_style(element) {
+function signed_percent_style(element) {
 	var text = element.innerText
-	if (text.match(/([\+\-])([0-9]{1,})\.[0-9]{1,}%/)) {
+	if (text.match(/([\+\-])([0-9]{1,}\.[0-9]{1,})%/)) {
 		var sign = RegExp.$1;
-		var percent = parseInt(RegExp.$2);
-		var color;
+		var percent = parseFloat(RegExp.$2);
+		var color = gradation(percent, 30);
 		var bgcolor;
-
-		if (percent < 1) {
-			color = "FF";
-		}
-
-		if (percent >= 1) {
-			color = "EE";
-		}
-		
-		if (percent > 3) {
-			color = "CC";
-		}
-
-		if (percent > 5) {
-			color = "AA";
-		}
-
-		if (percent > 10) {
-			color = "88";
-		}
-
-		if (percent > 15) {
-			color = "66";
-		}
-
-		if (percent > 20) {
-			color = "44";
-		}
-
 
 		if (sign == "+") {
 			bgcolor = "#" + color + color + "FF";
@@ -106,26 +53,27 @@ function percent_style(element) {
 function dividend_style(element) {
 	var text = element.innerText;
 
-	if (text.match(/^([0-9]{1,})\.[0-9]{1,}%/)) {
-		var percent = parseInt(RegExp.$1);
-		var color;
+	if (text.match(/^([0-9]{1,}\.[0-9]{1,})%/)) {
+		var percent = parseFloat(RegExp.$1);
+		var color = gradation(percent, 10);
 
-		if (percent < 1) {
-			color = "FF";
-		}
-
-		if (percent >= 1) {
-			color = "EE";
-		}
-		
-		if (percent > 3) {
-			color = "CC";
-		}
-
-		if (percent > 5) {
-			color = "AA";
-		}
 		element.setAttribute("bgcolor", "#" + color + color + "FF");
 	}
 	return element;
+}
+
+
+function gradation(value, max = 100) {
+	if (value > max) {
+		value = max;
+	}
+
+	var scale = 100.0 / max * 2
+	var color = parseInt((255 - value * scale)).toString(16);
+	console.log(color);
+	return color;
+}
+
+function gradation_desc(value, max = 100) {
+	return gradation(max - value, max);
 }
