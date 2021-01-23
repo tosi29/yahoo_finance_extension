072718fbac61994f8712facdad0205b6978d4406
tosi29 <tosi16@gmail.com>
2021-01-23 17:36:12 +0900

    Fix package.sh script

--- package.sh
+++ package.sh
@@ -3,8 +3,8 @@
 rm -rf dist
 mkdir -p dist dist/js
 
-cp -r css dist
-cp -r icon dist
+cp -R css dist
+cp -R icon dist
 cp manifest.json dist
 
 uglifyjs --compress --mangle -- js/script.js > dist/js/script.js
@@ -12,5 +12,5 @@ uglifyjs --compress --mangle -- js/tablesort_custom.js > dist/js/tablesort_custo
 
 (
     cd dist
-    zip yahoo_finance_extension.zip *
+    zip -r yahoo_finance_extension.zip *
 )
